cc89a15e2773825a8f740a5ebf661a71e52d89ef
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-12-23 21:27:05 +0530

    add welcome message to root endpoint

--- simple-hello-api/main.go
+++ simple-hello-api/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to the Simple Hello API")
+	})
+
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World")
 	})
